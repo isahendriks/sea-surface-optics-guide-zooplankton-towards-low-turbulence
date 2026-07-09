b76a3eea66bc5e10a33c75d835a685bcb07f195f
Isabel Hendriks <isabel.hendriks@biol.lu.se>
2026-07-09 13:58:30 +0200

    cleaned up spectral irradiance script

--- Simluated light regimes/Spectral Irradiance/analyze_irradiance.py
+++ Simluated light regimes/Spectral Irradiance/analyze_irradiance.py
@@ -59,18 +59,12 @@ def E_to_lux(E, wl, v_lambda):
 
     return lux
 
-# Load CIE photopic luminous efficiency function
-cie = np.loadtxt("CIE_sle_photopic.csv", delimiter=",", skiprows=1)
-
-cie_wavelengths = cie[:, 0]
-cie_v_lambda = cie[:, 1]
-
 # Load LIME moon spectrum
 lime = pd.read_csv("LIME_lunar_irradiance.csv", skiprows=22)
 
 lime.columns = ["Wavelength", "Irradiance", "Uncertainty"]
 
-#%% Read data test
+# Read data 
 wl= read_data('LEDmatrix_spectraldata/still.txt')[0]
 wlmin = 300 #min(wl)
 wlmax = 750 #max(wl)
@@ -93,7 +87,6 @@ lime_vis = lime[(lime["Wavelength"] >= wlmin) & (lime["Wavelength"] <= wlmax)]
 
 wl_lunar = lime_vis["Wavelength"].values
 E_lunar = lime_vis["Irradiance"].values * 1e6 # convert from W m^-2 nm^-1 to µW m^-2 nm^-1
-uncertainty_lunar = lime_vis["Uncertainty"].values * 1e6 # convert from W m^-2 nm^-1 to µW m^-2 nm^-1
 
 # read measurements
 still = read_data('LEDmatrix_spectraldata/still.txt')[1][imin:imax]
@@ -109,25 +102,14 @@ cal = cal_np[imin:imax,0]
 # Process measurements (subtract bkg (black), use calibration file, divide by 3 since measurement took 3 seconds)
 A = 0.1 * 0.1 # visible area of the LED matrix [m^2]
 
+# subtract black background counts from the measurements
 still_counts = (still - black)
 stormy_counts = (stormy - black)
 breeze_counts = (breeze - black)
 
-colors = sns.color_palette("Blues", 4)[1:]
+colors = sns.color_palette("Blues", 4)[1:4]
 
-# %% Plot spectra
-plt.plot(wl, still_counts, label = 'calm', color=colors[0])
-plt.plot(wl, breeze_counts, label = 'moderate', color=colors[1])
-plt.plot(wl, stormy_counts, label = 'rough', color=colors[2])
-plt.xlabel("Wavelength [nm]", fontsize=18)
-plt.ylabel("Intensity [$\mu$J]", fontsize=18)
-plt.grid(True)
-plt.legend()
-plt.tick_params(axis='both', labelsize=12)
-plt.xlim(wlmin, wlmax)
-
-# %% Calculate total light intensity
-
+### Calculate total light intensity
 # convert counts to energy flux (µJ s^-1 m^-2)/(µW m^-2) 
 E_still = still_counts * cal / 3 * A
 E_stormy = stormy_counts * cal / 3 * A 
@@ -158,37 +140,18 @@ I_breeze_E = np.trapz(E_breeze[(wl >= wl_min_zooplanton) & (wl <= wl_max_zooplan
 I_stormy_E = np.trapz(E_stormy[(wl >= wl_min_zooplanton) & (wl <= wl_max_zooplanton)], x=wl[(wl >= wl_min_zooplanton) & (wl <= wl_max_zooplanton)])
 I_lunar_E = np.trapz(E_lunar[(wl_lunar >= wl_min_zooplanton) & (wl_lunar <= wl_max_zooplanton)], x=wl_lunar[(wl_lunar >= wl_min_zooplanton) & (wl_lunar <= wl_max_zooplanton)])
 
-# print(f"I_still: {I_still_lux} lux, I_breeze: {I_breeze_lux} lux, I_stormy: {I_stormy_lux} lux")
-# print(f"I_still_PPDF: {I_still_PPDF} µmol m^-2 s^-1, I_breeze_PPDF: {I_breeze_PPDF} µmol m^-2 s^-1, I_stormy_PPDF: {I_stormy_PPDF} µmol m^-2 s^-1")
-
-# plt.figure(figsize=(7, 4))
-
-# plt.plot(wl, still_spec_PPDF*1e3, color=colors[0], linewidth=1, alpha=0.8, label=fr"Calm ({I_still_lux:.2f} lux)")
-# plt.plot(wl, breeze_spec_PPDF*1e3, color=colors[1], linewidth=1, alpha=0.8, label=fr"Moderate ({I_breeze_lux:.2f} lux)")
-# plt.plot(wl, stormy_spec_PPDF*1e3, color=colors[2], linewidth=1, alpha=0.8, label=fr"Rough ({I_stormy_lux:.2f} lux)")
-
-# plt.xlabel("Wavelength (nm)", fontsize=12)
-# plt.ylabel(r"Photon Flux Density  (nmol m$^{-2}$ s$^{-1}$ nm$^{-1}$)", fontsize=12)
-# plt.xlim(wlmin, wlmax)
-# plt.grid(True, alpha=0.3)
-# plt.tick_params(axis='both', labelsize=10)
-# plt.legend(loc="upper left", fontsize=10)
-# plt.tight_layout()
-# plt.savefig("LEDmatrix_spectraldata/spectral_intensity.png", dpi=600, bbox_inches="tight")
-# plt.savefig("LEDmatrix_spectraldata/spectral_intensity.svg", bbox_inches="tight")
-# plt.show()
-
-
+### Plot the spectral irradiance for each light regime and the lunar spectrum
 plt.figure(figsize=(7, 4))
 
+# Plot the spectral irradiance for each light regime 
 plt.plot(wl, E_still, color=colors[0], linewidth=1, alpha=0.8, label=fr"Calm")
 plt.plot(wl, E_breeze, color=colors[1], linewidth=1, alpha=0.8, label=fr"Moderate")
 plt.plot(wl, E_stormy, color=colors[2], linewidth=1, alpha=0.8, label=fr"Rough")
 
+# Plot the lunar spectrum
 plt.plot(wl_lunar, E_lunar, color='gray', linewidth=1, alpha=0.8, label=fr"Lunar Spectrum")
-# plt.fill_between(wl_lunar, E_lunar - uncertainty_lunar, E_lunar + uncertainty_lunar, color='gray', alpha=0.2, label=fr"Uncertainty")
 
-# plot rectangle for zooplankton relevant range
+# Highlight the zooplankton relevant range
 plt.axvspan(wl_min_zooplanton, wl_max_zooplanton, color='grey', alpha=0.1, label=fr"Zooplankton relevant range")
 
 plt.xlabel("Wavelength (nm)", fontsize=12)
@@ -207,5 +170,3 @@ print(f"Total light intensity in the zooplankton relevant range ({wl_min_zooplan
       f"Moderate: {I_breeze_E*1e-3:.2f} mW/m2\n",
       f"Rough: {I_stormy_E*1e-3:.2f} mW/m2\n",
       f"Lunar: {I_lunar_E*1e-3:.2f} mW/m2")
-
-# %%
